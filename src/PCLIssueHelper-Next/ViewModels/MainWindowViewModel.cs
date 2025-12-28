5493376a329328bef3cb3a59d7e875886e69e4a8
Hill23333 <a18035135032@163.com>
2025-12-28 15:47:12 +0800

    ref: 新开始

---
+++ src/PCLIssueHelper-Next/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PCLIssueHelper_Next.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/PCLIssueHelper-Next/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PCLIssueHelper_Next.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
